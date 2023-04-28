11bb83af120bbe2d7be5f20cd6b0cd5e1b91e76f
th3nd <45330378+th3nd@users.noreply.github.com>
2023-04-28 11:25:47 +0200

    upd

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ function menu_state(arg) {
     menu.style.display = arg ? 'block' : 'none';
 }
 
+
+const content = document.querySelector('#content');
+const feed = document.querySelector('.feed');
+
+String.prototype.remove = function (s){return this.replace(s,'')}
+
 current_block = 0;
-
+saved_notes = ''
 function submit() {
-    const content = document.querySelector('#content');
-
-    const feed = document.querySelector('.feed');
+    // check saved notes and get how many of the blocks that exists
+    saved_notes = localStorage.getItem('notes');
+    if (saved_notes)
+        current_block = (saved_notes.match(/id/g) || []).length;
 
     if (!content.value) {
         content.placeholder = 'content is missing a value';
     }   else {
         menu_state(false);
         // inte de bästa sättet
-        feed.innerHTML += "<div class='block' onclick='select("+current_block+")' id='"+current_block+"'><p>"+content.value+"</p><img width=20 src='data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pjxzdmcgdmlld0JveD0iMCAwIDMyIDMyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxkZWZzPjxzdHlsZT4uY2xzLTF7ZmlsbDpub25lO3N0cm9rZTojMDAwO3N0cm9rZS1saW5lY2FwOnJvdW5kO3N0cm9rZS1saW5lam9pbjpyb3VuZDtzdHJva2Utd2lkdGg6MnB4O308L3N0eWxlPjwvZGVmcz48dGl0bGUvPjxnIGRhdGEtbmFtZT0iOTMtYmluIiBpZD0iXzkzLWJpbiI+PHJlY3QgY2xhc3M9ImNscy0xIiBoZWlnaHQ9IjIyIiB3aWR0aD0iMTgiIHg9IjciIHk9IjkiLz48cmVjdCBjbGFzcz0iY2xzLTEiIGhlaWdodD0iNCIgd2lkdGg9IjIyIiB4PSI1IiB5PSI1Ii8+PHBvbHlsaW5lIGNsYXNzPSJjbHMtMSIgcG9pbnRzPSIxMiA1IDEyIDEgMjAgMSAyMCA1Ii8+PGxpbmUgY2xhc3M9ImNscy0xIiB4MT0iMTMiIHgyPSIxMyIgeTE9IjE0IiB5Mj0iMzEiLz48bGluZSBjbGFzcz0iY2xzLTEiIHgxPSIxOSIgeDI9IjE5IiB5MT0iMTQiIHkyPSIzMSIvPjwvZz48L3N2Zz4='</div>";
+        feed.innerHTML += "<div class='block' onclick='select("+current_block+")' id='"+current_block+"'><p>"+content.value+"</p><img width=20 onclick='delete_block("+current_block+")'src='data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pjxzdmcgdmlld0JveD0iMCAwIDMyIDMyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxkZWZzPjxzdHlsZT4uY2xzLTF7ZmlsbDpub25lO3N0cm9rZTojMDAwO3N0cm9rZS1saW5lY2FwOnJvdW5kO3N0cm9rZS1saW5lam9pbjpyb3VuZDtzdHJva2Utd2lkdGg6MnB4O308L3N0eWxlPjwvZGVmcz48dGl0bGUvPjxnIGRhdGEtbmFtZT0iOTMtYmluIiBpZD0iXzkzLWJpbiI+PHJlY3QgY2xhc3M9ImNscy0xIiBoZWlnaHQ9IjIyIiB3aWR0aD0iMTgiIHg9IjciIHk9IjkiLz48cmVjdCBjbGFzcz0iY2xzLTEiIGhlaWdodD0iNCIgd2lkdGg9IjIyIiB4PSI1IiB5PSI1Ii8+PHBvbHlsaW5lIGNsYXNzPSJjbHMtMSIgcG9pbnRzPSIxMiA1IDEyIDEgMjAgMSAyMCA1Ii8+PGxpbmUgY2xhc3M9ImNscy0xIiB4MT0iMTMiIHgyPSIxMyIgeTE9IjE0IiB5Mj0iMzEiLz48bGluZSBjbGFzcz0iY2xzLTEiIHgxPSIxOSIgeDI9IjE5IiB5MT0iMTQiIHkyPSIzMSIvPjwvZz48L3N2Zz4='</div>";
         content.value = '';
-        current_block++;
+        // current_block++;
+        save_notes()
     }
 }
 
@@ -37,4 +45,20 @@ function select(a) {
 
 
     blocks[a] = !blocks[a];
+}
+
+function delete_block(a) {
+    const block = document.getElementById(a);
+    block.style.display = 'none';
+}
+
+
+function save_notes() {
+    localStorage.setItem('notes', feed.innerHTML);
+}
+
+get_notes();
+function get_notes() {
+    // saved_notes = localStorage.getItem('notes');
+    feed.innerHTML = localStorage.getItem('notes');
 }
---
+++ scrollevent.js
@@ -0,0 +1,8 @@
+// let bg = document.querySelector('.img-bg')
+
+// bg.style.opacity = 1
+
+
+// window.addEventListener('scroll',function(e){
+//     bg.style.opacity -= .1
+// })
